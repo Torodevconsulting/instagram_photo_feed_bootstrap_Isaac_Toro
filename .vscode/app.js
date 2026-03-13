ee7869590d726089f9b95a2ba59f2dcabe79fa08
Your Name <torodevelopmentbcn@gmail.com>
2026-03-13 19:23:47 +0100

    Add initial HTML, CSS, and JavaScript files for the project

---
+++ .vscode/app.js
@@ -0,0 +1,6 @@
+import "./style.css";
+
+const titulo = document.querySelector(".titulo");
+const parrafos = document.querySelectorAll(".parrafo");
+const boton = document.querySelector("button");
+const input = document.querySelector("#nombreInput");
